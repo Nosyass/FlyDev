0300dccd0dc3b4b54c0a647ba822377b16303d97
Noyass <yassin.sahli@esprit.tn>
2015-12-13 17:31:03 +0100

    Signed-off-by: Noyass <yassin.sahli@esprit.tn>

---
+++ AcademieDuLac/src/main/java/tn/esprit/domain/Admin.java
@@ -0,0 +1,33 @@
+package tn.esprit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+	private  int id ;
+private String Login ;
+private String Password ;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+public String getLogin() {
+	return Login;
+}
+public void setLogin(String login) {
+	Login = login;
+}
+public String getPassword() {
+	return Password;
+}
+public void setPassword(String password) {
+	Password = password;
+}}
